a68c18752e5e19306f1c2ec7c3d713bb1f4fb567
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-30 15:32:25 +0800

    first

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import Loginpage from "./pages/LoginPage";
+import Registerpage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage"
+import EditPost from "./pages/EditPost"
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/register" element={<Registerpage />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
+}
+export default App;
