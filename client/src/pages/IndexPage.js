e413614cfb7c5db64b0ce9acd8e52969afcc8219
LouisKok <bluecrab_888@hotmail.com>
2023-07-05 17:16:07 +0800

    Update IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -5,7 +5,7 @@ import { useEffect, useState} from "react"
 export default function IndexPage() {
     const [posts, setPosts] = useState([])
     useEffect(() => {
-        fetch('http://localhost:4000/post').then(response => {
+        fetch('https://mern-2-api.onrender.com/post').then(response => {
             response.json().then(posts => {
                 console.log(posts)
                 setPosts(posts)
