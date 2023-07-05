dbc72369d5684921759dca2960776fdcfe328cfc
LouisKok <bluecrab_888@hotmail.com>
2023-07-05 16:29:16 +0800

    Update Post.js

--- client/src/Post.js
+++ client/src/Post.js
@@ -22,7 +22,7 @@ export default function Post({
           <h2>{title}</h2>
         </Link>
         <p className="info">
-          <a className="author">{author.username}</a>
+          <a href="/" className="author">{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className="summary">{summary}</p>
