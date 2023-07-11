5b57c66398fb9b0eab2bfe7fdf05ec39358eef05
LouisKok <bluecrab_888@hotmail.com>
2023-07-11 16:36:04 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -39,6 +39,14 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
@@ -55,14 +63,6 @@ app.post("/login", async (req, res) => {
   } else {
     res.status(400).json("wrong credential ");
   }
-});
-
-app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, (err, info) => {
-    if (err) throw err;
-    res.json(info);
-  });
 });
 
 app.post("/logout", (req, res) => {
